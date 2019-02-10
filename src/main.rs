7f09ee11e137cc4ef312c38429695f759e29cf9d
むずでょ <muzudho1@gmail.com>
2019-02-11 00:26:34 +0900

    すぐ投了☆（＾～＾）

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::io;
+
+/// Computer shogi engine Kifuwarabe for WCSC29.
+///
+/// Windows 10.
+/// 
+/// [Windows] + [R].
+/// `cmd`, [Enter].
+/// 
+/// ```Shell
+/// ### Example.
+/// cd C:\muzudho\projects_rust\rust-kifuwarabe-wcsc29
+/// cls
+/// 
+/// ### Compile.
+/// cargo clippy
+/// 
+/// ### Run.
+/// cargo run --release
+/// ```
+/// 
+/// Execution file.
+/// C:/muzudho/projects_rust/rust-kifuwarabe-wcsc29/target/release/rust-kifuwarabe-wcsc29.exe
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Standard input.
+        // Be sure to add "info" before the output message.
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("info Failed: stdin.read_line.");
+
+        // Excludes trailing newlines. The surrounding whitespace deletes.
+        line = line.trim()
+            .parse()
+            .expect("info Failed: stdin parse.");
+
+        if line == "quit" {
+            break;
+        } else if line == "usi" {
+            println!("id name Kifuwarabe WCSC29");
+            println!("id author Satoshi TAKAHASHI");
+            println!("usiok");
+        } else if line == "isready" {
+            println!("readyok");
+        } else if line == "usinewgame" {
+        } else if line.starts_with("position") {
+            println!("info What is position?");
+        } else if line.starts_with("go") {
+            println!("bestmove resign");
+        }
+    }
 }
