a54b0cbdc3fea862f302b7ac9fce25634bc8c090
むずでょ <muzudho1@gmail.com>
2019-03-25 22:54:04 +0900

    デバッグ出力中☆（＾～＾）

--- src/csa_conv/csa_converter.rs
+++ src/csa_conv/csa_converter.rs
@@ -13,7 +13,11 @@ pub struct CsaConverter {
 }
 impl CsaConverter {
     /// 変換には、現局面が必要。
-    pub fn convert_move(cmove:&CsaMove, position:&Position) -> Vec<PhysicalMove> {
+    pub fn convert_move(
+        comm:&Communication,
+        cmove:&CsaMove,
+        position:&Position,
+        ply:i16) -> Vec<PhysicalMove> {
         let mut p_moves = Vec::new();
 
         let destination_address = Address::create_by_cell(
@@ -22,63 +26,62 @@ impl CsaConverter {
             position.get_board_size()
         );
         
-        match cmove.get_drop()
-        {
-            Some(drop) => {
-                // 駒を打つ動きの場合
+        if let Some(drop) = cmove.get_drop() {
+            // 駒を打つ動きの場合
+
+            // hand-off
+            let hand_off = PhysicalMove::create_by_address(Address::create_by_hand(Some(position.get_phase()), drop));
+            p_moves.push(hand_off);
+
+            // hand-on
+            let hand_on = PhysicalMove::create_by_address(destination_address);
+            p_moves.push(hand_on);
+        } else {
+            // 駒を進める動きの場合
+            if let Some(capture_piece) = position.get_piece_by_address(destination_address.get_index()) {
+                // 駒を取る動きが入る場合
+                comm.println(&format!("[{}] 駒を取る動きが入る場合 {}", ply, piece_to_sign(Some(capture_piece))));
 
                 // hand-off
-                let hand_off = PhysicalMove::create_by_address(Address::create_by_hand(Some(position.get_phase()), drop));
+                let hand_off = PhysicalMove::create_by_address(destination_address);
                 p_moves.push(hand_off);
 
+                // hand-turn
+                if is_promotion_piece(Some(capture_piece)) {
+                    let hand_turn = PhysicalMove::turn_over();
+                    p_moves.push(hand_turn);
+                }
+
+                // hand-rotate
+                let hand_rotate = PhysicalMove::rotate();
+                p_moves.push(hand_rotate);
+
                 // hand-on
-                let hand_on = PhysicalMove::create_by_address(destination_address);
+                let up = piece_to_piece_type(capture_piece);
+                let hand_on = PhysicalMove::create_by_address(Address::create_by_hand(Some(position.get_phase()), up));
                 p_moves.push(hand_on);
-            },
-            None => {
-                // 駒を進める動きの場合
-                if let Some(piece) = position.get_piece_by_address(destination_address.get_index()) {
-                    // 駒を取る動きが入る場合
+            } else {
+                comm.println(&format!("[{}] 駒は取らない", ply));
+            }
 
-                    // hand-off
-                    let hand_off = PhysicalMove::create_by_address(destination_address);
-                    p_moves.push(hand_off);
+            // board-off
+            let board_off = PhysicalMove::create_by_address(Address::create_by_cell(
+                cmove.source_file,
+                cmove.source_rank,
+                position.get_board_size()
+            ));
+            p_moves.push(board_off);
 
-                    // hand-turn
-                    if is_promotion_piece(Some(piece)) {
-                        let hand_turn = PhysicalMove::turn_over();
-                        p_moves.push(hand_turn);
-                    }
+            // board-turn-over
+            if cmove.promotion {
+                let board_turn = PhysicalMove::turn_over();
+                p_moves.push(board_turn);
+            }
 
-                    // hand-rotate
-                    let hand_rotate = PhysicalMove::rotate();
-                    p_moves.push(hand_rotate);
-
-                    // hand-on
-                    let up = piece_to_piece_type(piece);
-                    let hand_on = PhysicalMove::create_by_address(Address::create_by_hand(Some(position.get_phase()), up));
-                    p_moves.push(hand_on);
-                }
-
-                // board-off
-                let board_off = PhysicalMove::create_by_address(Address::create_by_cell(
-                    cmove.source_file,
-                    cmove.source_rank,
-                    position.get_board_size()
-                ));
-                p_moves.push(board_off);
-
-                // board-turn-over
-                if cmove.promotion {
-                    let board_turn = PhysicalMove::turn_over();
-                    p_moves.push(board_turn);
-                }
-
-                // board-on
-                let board_on = PhysicalMove::create_by_address(destination_address);
-                p_moves.push(board_on);
-            },
-        }
+            // board-on
+            let board_on = PhysicalMove::create_by_address(destination_address);
+            p_moves.push(board_on);
+        };
 
         // change-phase
         let change_phase = PhysicalMove::change_phase();
@@ -94,14 +97,19 @@ impl CsaConverter {
         c_record:&CsaRecord,
         physical_record:&mut PhysicalRecord) {
 
+        let mut ply = 1;
         for cmove in &c_record.items {
             let p_moves = CsaConverter::convert_move(
+                comm,
                 cmove,
-                position);
+                position,
+                ply);
 
-            for physical_move in p_moves {
-                CommonOperation::go(comm, physical_record, &physical_move, position);
+            for pmove in p_moves {
+                CommonOperation::go(comm, physical_record, &pmove, position);
             }
+
+            ply += 1;
         }
     }
 }
