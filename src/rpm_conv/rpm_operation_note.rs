2a283b2931d7bb8b23c33283a596e8814bd8aad0
むずでょ <muzudho1@gmail.com>
2019-04-10 21:16:32 +0900

    parse ope note

--- src/rpm_conv/rpm_operation_note.rs
+++ src/rpm_conv/rpm_operation_note.rs
@@ -1,6 +1,10 @@
 use address::*;
 use position::*;
 
+use communication::*;
+use parser::*;
+use piece_etc::*;
+
 /// Vector に入れるときコピーする。
 #[derive(Clone, Copy, PartialEq)]
 pub struct RpmOpeNote {
@@ -86,6 +90,100 @@ impl RpmOpeNote {
                     panic!("Unexpected physical move print.")
                 }
             },
+        }
+    }
+
+    /// ノート１つ読取☆（＾～＾）
+    pub fn parse_1note(comm:&Communication, line:&str, start:&mut usize, position:&mut Position) -> Option<RpmOpeNote> {
+        let ch1 = line[*start..=*start].chars().nth(0).unwrap();
+        match ch1 {
+            ' ' => {
+                comm.print(&ch1.to_string());
+                *start += 1;
+                None
+            }
+            '0' => {
+                // 駒台。
+                *start += 1;
+
+                let ch2 = line[*start..=*start].chars().nth(0).unwrap();
+                *start += 1;
+
+                let text15;
+                match ch2 {
+                    'P' | 'p' | 'ﾅ' => {
+                        // 成り駒は、不成駒と同じところに置くので、成りのマークは読み飛ばす。
+                        text15 = line[*start..=*start].chars().nth(0).unwrap().to_string();
+                        *start += 1;
+                    },
+                    _ => {
+                        // Ignored.
+                        text15 = "".to_string();
+                    },
+                };
+
+                // 駒の種類、フェーズ。
+                let piece = PhysicalSign::default(ch2.to_string()).to_piece();
+
+                comm.print(&format!("{}{}{}", ch1, text15, ch2));
+                let address = Address::create_by_hand(
+                    piece_to_phase(Some(piece)),
+                    piece_to_piece_type(piece));
+                comm.println(&format!("address index = {}.", address.get_index()));
+                Some(RpmOpeNote::create_by_address(address))
+            },
+            '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                // セル
+                *start += 1;
+                let ch2 = line[*start..=*start].chars().nth(0).unwrap();
+                *start += 1;
+                comm.print(&format!("{}{}", ch1, ch2));
+                let file = Parser::file_char_to_i8(ch1);
+                let rank = Parser::rank_char_to_i8(ch2);
+                let address = Address::create_by_file_rank(file, rank, position.get_board_size());
+                Some(RpmOpeNote::create_by_address(address))
+            },
+            '+' => {
+                // 成り。
+                comm.print(&ch1.to_string());
+                *start += 1;
+                Some(RpmOpeNote::turn_over())
+            },
+            '-' => {
+                // １８０°回転。
+                comm.print(&ch1.to_string());
+                *start += 1;
+                Some(RpmOpeNote::rotate())
+            },
+            '|' => {
+                // フェーズ交代。
+                comm.print(&ch1.to_string());
+                *start += 1;
+                Some(RpmOpeNote::change_phase())
+            },
+            '[' => {
+                // フェーズ交代。 ']' まで読み飛ばす。
+                comm.print(&ch1.to_string());
+                *start += 1;
+                loop {
+                    if line.len() <= *start {
+                        break;
+                    }
+                    
+                    let sub_ch = line[*start..=*start].chars().nth(0).unwrap();
+                    comm.print(&sub_ch.to_string());
+                    *start += 1;
+
+                    if sub_ch == ']' {
+                        break;
+                    }
+                };
+                Some(RpmOpeNote::change_phase())
+            },
+            _ => {
+                let last = line.len();
+                panic!("Unexpected line '{}'.", &line[*start..last]);
+            }
         }
     }
 }
--- src/rpm_conv/rpm_record.rs
+++ src/rpm_conv/rpm_record.rs
@@ -3,11 +3,8 @@ use rpm_conv::rpm_identify_track::*;
 use rpm_conv::rpm_operation_track::*;
 use rpm_conv::rpm_operation_note::*;
 
-use address::*;
 use common_operation::*;
 use communication::*;
-use piece_etc::*;
-use parser::*;
 
 /// 対局情報。
 pub struct RpmRecordHeader {
@@ -128,98 +125,10 @@ impl RpmRecord {
                 return;
             }
 
-            let ch1 = line[start..=start].chars().nth(0).unwrap();
-            let rpm_note_opt = match ch1 {
-                ' ' => {
-                    comm.print(&ch1.to_string());
-                    start += 1;
-                    None
-                }
-                '0' => {
-                    // 駒台。
-                    start += 1;
+            let rpm_ope_1note_opt = RpmOpeNote::parse_1note(&comm, &line, &mut start, position);
 
-                    let ch2 = line[start..=start].chars().nth(0).unwrap();
-                    start += 1;
 
-                    let text15;
-                    match ch2 {
-                        'P' | 'p' | 'ﾅ' => {
-                            // 成り駒は、不成駒と同じところに置くので、成りのマークは読み飛ばす。
-                            text15 = line[start..=start].chars().nth(0).unwrap().to_string();
-                            start += 1;
-                        },
-                        _ => {
-                            // Ignored.
-                            text15 = "".to_string();
-                        },
-                    };
-
-                    // 駒の種類、フェーズ。
-                    let piece = PhysicalSign::default(ch2.to_string()).to_piece();
-
-                    comm.print(&format!("{}{}{}", ch1, text15, ch2));
-                    let address = Address::create_by_hand(
-                        piece_to_phase(Some(piece)),
-                        piece_to_piece_type(piece));
-                    comm.println(&format!("address index = {}.", address.get_index()));
-                    Some(RpmOpeNote::create_by_address(address))
-                },
-                '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                    // セル
-                    start += 1;
-                    let ch2 = line[start..=start].chars().nth(0).unwrap();
-                    start += 1;
-                    comm.print(&format!("{}{}", ch1, ch2));
-                    let file = Parser::file_char_to_i8(ch1);
-                    let rank = Parser::rank_char_to_i8(ch2);
-                    let address = Address::create_by_file_rank(file, rank, position.get_board_size());
-                    Some(RpmOpeNote::create_by_address(address))
-                },
-                '+' => {
-                    // 成り。
-                    comm.print(&ch1.to_string());
-                    start += 1;
-                    Some(RpmOpeNote::turn_over())
-                },
-                '-' => {
-                    // １８０°回転。
-                    comm.print(&ch1.to_string());
-                    start += 1;
-                    Some(RpmOpeNote::rotate())
-                },
-                '|' => {
-                    // フェーズ交代。
-                    comm.print(&ch1.to_string());
-                    start += 1;
-                    Some(RpmOpeNote::change_phase())
-                },
-                '[' => {
-                    // フェーズ交代。 ']' まで読み飛ばす。
-                    comm.print(&ch1.to_string());
-                    start += 1;
-                    loop {
-                        if line.len() <= start {
-                            break;
-                        }
-                        
-                        let sub_ch = line[start..=start].chars().nth(0).unwrap();
-                        comm.print(&sub_ch.to_string());
-                        start += 1;
-
-                        if sub_ch == ']' {
-                            break;
-                        }
-                    };
-                    Some(RpmOpeNote::change_phase())
-                },
-                _ => {
-                    let last = line.len();
-                    panic!("Unexpected line '{}'.", &line[start..last]);
-                }
-            };
-
-            if let Some(rpm_note) = rpm_note_opt {
+            if let Some(rpm_note) = rpm_ope_1note_opt {
                 CommonOperation::touch_talking_beautifle_world(comm, rpm_record, &rpm_note, position);
             }
         }
